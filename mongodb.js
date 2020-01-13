6a31b6035059df527840ce330f76d971750276be
Anubhav <anubhavnitp@gmail.com>
2020-01-14 02:59:19 +0530

    second commit

--- mongodb.js
+++ mongodb.js
@@ -3,17 +3,28 @@
 // Mongodb server using node.js
 
 
-const mongodb = require('mongodb')
+const mongodb = require('mongodb') //npm library
 const MongoClient = mongodb.MongoClient
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
-const databaseName = 'task-manager'
+const dataBaseName = 'task-manager'
 
-MongoClient.connect(connectionURL, { useNewUrlParser:true,useUnifiedTopology:true }, (error, client) => {
+MongoClient.connect(connectionURL, { useNewUrlParser:true, useUnifiedTopology:true }, (error, client) => {
     if(error) {
-        console.log('Unable to connect to database!')
+      return  console.log('Unable to connect to database!')
     } 
 
-    console.log('Connected Perfectly!')
+    const db = client.db(dataBaseName)
+
+    db.collection('users').insertOne({
+        name : 'Anubhav',
+        age : 21
+    },(error,result) => {
+        if(error) {
+            return console.log('Unable to insert user')
+        }
+
+        console.log(result.ops)
+    })
 })
 
